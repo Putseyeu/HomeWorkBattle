8a3dc42d320ce04481eb1c18044a0c0efeb91f38
Putseyeu <putseyeu.a.d@gmail.com>
2021-11-28 14:53:15 +0100

    step 1

---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float healthBoss = 500;
+            float healthPlayer = 300;
+            string nameBoss = "Boss";
+            string namePlayer;
+            float fireBall = 50;
+            float iceArrow = 30;
+            float electricWhip = 20;
+            Random rand = new Random();
+            float amplifyMagic;
+            string inputPlayer;
+            float damageBoss;
+
+            Console.WriteLine("Здравствуй путник! Назови своё имя, сразившись со мной и победив меня " +
+                "сможешь пройти дальше!");
+            namePlayer = Console.ReadLine();
+            Console.WriteLine($"Ну что {namePlayer} я великий {nameBoss} и сегодня ты почувствуешь горечь поражения!!!");
+
+            while (healthBoss > 0 && healthPlayer > 0)
+            {
+                Console.WriteLine($"Уровень здоровья {namePlayer} {healthPlayer}, здоровье {nameBoss} {healthBoss}");
+                Console.WriteLine("Выберети заклинания из вашей книги заклинаний!!!\n" +
+                    "1 - Огненый шар. Не возможно уклониться, но может нанести урон на заклинателя\n" +
+                    "2 - Ледяная Стрела. Противник может уклониться, будьте внимательны частично урон может попасть по вам.\n" +
+                    "3 - Электрический кнут.Безопастно для заклинателя, но наносит меньший урон. Может ослепить противника.\n");
+                
+                string spellPlayer = Console.ReadLine();
+
+                if (spellPlayer == "1")
+                {
+                    Console.WriteLine("Хотите усилить заклинание? произнесите святое заклинание ЯЮНИОР ");
+                    inputPlayer = Console.ReadLine();
+                    if (inputPlayer == "ЯЮНИОР")
+                    {
+                        amplifyMagic = rand.Next(20, 41);
+                        Console.WriteLine($"{namePlayer} сила вашего заклинания увеличина на {amplifyMagic} едениц");
+                    }
+                    else
+                    {
+                        amplifyMagic = 0;
+                    }
+
+                    Console.WriteLine("Произносите заклинания и на противника летит огненый шар!");
+                    int battle = rand.Next(0, 2);
+                    if (battle == 0)
+                    {
+                        healthBoss -=fireBall + amplifyMagic;
+                        Console.WriteLine($"{namePlayer} нанес урон {nameBoss} {fireBall + amplifyMagic} единиц урона");
+                        damageBoss = rand.Next(30, 50);
+                        healthPlayer -= damageBoss;
+                        Console.WriteLine($"{nameBoss} наносит ответный урон {damageBoss}");
+                    }
+                    else
+                    {
+                        healthBoss -= fireBall + amplifyMagic;
+                        healthPlayer -= fireBall + amplifyMagic;
+                        Console.WriteLine($"{namePlayer} нанес урон {fireBall + amplifyMagic} единиц урона, " +
+                            $"но огонь зацепил и вас!" +
+                            $"вы также получили {fireBall + amplifyMagic} единиц урона");
+                    }
+
+                }
+                else if (spellPlayer == "2")
+                {
+                    Console.WriteLine("Хотите усилить заклинание? произнесите святое заклинание ЯЮНИОР ");
+                    inputPlayer = Console.ReadLine();
+                    if (inputPlayer == "ЯЮНИОР")
+                    {
+                        amplifyMagic = rand.Next(20, 41);
+                        Console.WriteLine($"{namePlayer} сила вашего заклинания увеличина на {amplifyMagic} едениц");
+                    }
+                    else
+                    {
+                        amplifyMagic = 0;
+                    }
+
+                    Console.WriteLine("Произносите заклинания и на противника летит ледяная стрела!");
+                    int battle = rand.Next(0, 2);
+                    if (battle == 0)
+                    {
+                        healthBoss -= iceArrow + amplifyMagic;
+                        Console.WriteLine($"{namePlayer} нанёс урон {nameBoss} {iceArrow + amplifyMagic} единиц урона");
+                        Console.WriteLine($"{nameBoss} холодно, противник замедлился и вы увернулись от урона. ");
+                    }
+                    else
+                    {
+                        healthBoss -= iceArrow + amplifyMagic;
+                        float percentageCold = rand.Next(25, 50);
+                        healthPlayer -= (iceArrow + amplifyMagic) * (percentageCold / 100);
+                        Console.WriteLine($"{namePlayer} нанес урон {nameBoss} {iceArrow + amplifyMagic} единиц урона, " +
+                            $"но холод зацепил и  вас!" +
+                            $"нанеся  {(iceArrow + amplifyMagic) * (percentageCold / 100)} единиц урона");
+                        damageBoss = rand.Next(30, 50);
+                        Console.WriteLine($"{nameBoss} наносит по {namePlayer} ответный урон {damageBoss} единиц урона");
+                    }
+                }
+
+                else if (spellPlayer == "3")
+                {
+                    Console.WriteLine("Хотите усилить заклинание? произнесите святое заклинание ЯЮНИОР ");
+                    inputPlayer = Console.ReadLine();
+                    if (inputPlayer == "ЯЮНИОР")
+                    {
+                        amplifyMagic = rand.Next(20, 41);
+                        Console.WriteLine($"{namePlayer} сила вашего заклинания увеличина на {amplifyMagic} едениц");
+                    }
+                    else
+                    {
+                        amplifyMagic = 0;
+                    }
+                    Console.WriteLine("Произносите заклинания и бьёте противника электрическим кнутом.");
+                    int battle = rand.Next(0, 2);
+                    if (battle == 0)
+                    {
+                        healthBoss -= electricWhip + amplifyMagic;
+                        Console.WriteLine($"{namePlayer} наносит {electricWhip+amplifyMagic} урона!");
+                        battle = rand.Next(0, 2);
+                        if(battle == 0)
+                        {
+                            Console.WriteLine($"{nameBoss} ослеплен и не может нанести урон {namePlayer}");
+                        }
+                        else
+                        {
+                            damageBoss = rand.Next(30, 50);
+                            healthPlayer -= damageBoss;
+                            Console.WriteLine($"{nameBoss} наносит ответный удар и нанёс {damageBoss} единиц урона!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nameBoss} увернулся и готов нанести ответный удар!");
+                        battle = rand.Next(0, 2);
+                        if (battle == 0)
+                        {
+                            damageBoss = rand.Next(30, 50);
+                            healthPlayer -= damageBoss;
+                            Console.WriteLine($"{nameBoss} наносит {namePlayer}  {damageBoss} урона.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{namePlayer} увернулся от удара и не получает урон!");
+                        }
+                    }
+                }
+            }
+
+            if (healthBoss <= 0 && healthPlayer <=0)
+            {
+                Console.WriteLine($"{nameBoss} и  {namePlayer} пали в бою. Побидителя нету! ");
+            }
+            else if(healthPlayer <= 0)
+            {
+                Console.WriteLine($"{nameBoss} сразил {namePlayer}.");
+            }
+            else if(healthBoss <= 0)
+            {
+                Console.WriteLine($"{namePlayer} победил {nameBoss} в этой битве.");
+            }
+        }
+    }
+}
